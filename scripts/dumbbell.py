a8858c7d8e39927b39e3a31172eb1e6e99551f1c
Yosef Meller <mellerf@netvision.net.il>
2015-11-03 16:25:09 +0200

    Mark multiple cameras

--- scripts/dumbbell.py
+++ scripts/dumbbell.py
@@ -8,26 +8,38 @@ from math import floor, ceil
 import matplotlib.pyplot as pl, matplotlib.cm as cm
 from matplotlib.widgets import  RectangleSelector
 
-rects = []
-tiles = []
-def onselect(eclick, erelease):
+def onselect(eclick, erelease, im, rects, tiles):
     xs = np.sort(np.r_[eclick.xdata, erelease.xdata])
     ys = np.sort(np.r_[eclick.ydata, erelease.ydata])
     rects.append(np.c_[xs, ys].flatten())
+    print rects[-1]
     tiles.append(im[floor(ys[0]):ceil(ys[1]), floor(xs[0]):ceil(xs[1])])
+    if len(rects) == 2:
+        pl.close()
+
+def mark_image(image_path):
+    rects = []
+    tiles = []
+
+    pl.figure()
+    ax = pl.subplot('111')
+
+    im = pl.imread(image_path)
+    ax.imshow(im, cmap=cm.gray)
+    pl.title(image_path)
     
-fig = pl.figure()
-ax = pl.subplot('111')
+    sel_style = {'edgecolor': 'red', 'fill': False}
+    rs = RectangleSelector(ax, lambda clk, rls: onselect(clk, rls, im, rects, tiles), 
+        rectprops=sel_style)
+    rs.set_active(True)
 
-im = pl.imread('data/20150714/dumbbell_end/20150714_Scene15_1-DVR Express CLFC_3136.TIF')
-ax.imshow(im, cmap=cm.gray)
+    pl.show()
+    return rects, tiles
 
-sel_style = {'edgecolor': 'red', 'fill': False}
-rs = RectangleSelector(ax, onselect, rectprops=sel_style)
-rs.set_active(True)
+for cam in xrange(4):
+    r, t = mark_image('data/20150714/dumbbell_end/cam%d.3136' % (cam + 1))
+    print cam
+    print r[0]
+    print r[1]
 
-pl.show()
-
-pl.figure()
-pl.imshow(tiles[-1], cmap=cm.gray)
-pl.show()
+    
